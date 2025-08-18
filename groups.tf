5f4c63024e17f5124a3321c2d47c5eb9a52f7b73
shyamchaganti <98919556+shyamchaganti@users.noreply.github.com>
2025-08-18 19:15:27 -0400

    Update groups.tf

--- groups.tf
+++ groups.tf
@@ -34,15 +34,4 @@ resource "azuread_group_member" "engineers" {
   member_object_id = each.value.id
 }
 
-resource "azuread_group" "customer_success" {
-  display_name = "Education - Customer Success"
-  security_enabled = true
-}
-
-resource "azuread_group_member" "customer_success" {
-  for_each = { for u in azuread_user.users: u.mail_nickname => u if u.job_title == "Customer Success" }
-
-  group_object_id  = azuread_group.customer_success.id
-  member_object_id = each.value.id
-}
 
